809c4fd4f0a95774854d4cf8f67c93f625e48d7d
Admin <Admin@TEB106008>
2023-01-13 14:42:46 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många sporter vill du skriva in?");
+            int Sportantal = int.Parse(Console.ReadLine());
+            string[] sport = new string[Sportantal];
+            int x = 0;
+            
+            for (int i = 0; i < sport.Length; i++)
+            {
+                Console.WriteLine("Skriv in sporterna med deras namn");
+                sport[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Det här är sporterna du skrev in");
+            for (int i = 0; i < sport.Length; i++)
+            {
+                Console.Write(" "+sport[i]);
+            }         
+        }
+    }
+}
